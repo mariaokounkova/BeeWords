a538258f4ab4434025f02c2afcf6d7f2367076ee
Maria Okounkova <m.okounkova@gmail.com>
2023-05-11 17:14:42 -0700

    Adding word finding
    Add FindWords.py, which uses the dictionary in dictionary.txt
    to find the longest words without `s` and with 7 or fewer letters

---
+++ FindWords.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import numpy as np  
+
+## Read in dictionary
+dictionary = np.genfromtxt('dictionary.txt', dtype='str')
+print("%d words in dictionary" % len(dictionary))
+
+## Subset of words without 's' and wirth 7 or fewer letters
+bee_words = [word for word in dictionary if \
+             's' not in word and len(set(word)) <= 7]
+
+print("%d possible words found" % len(bee_words))
+
+## Print out the longest words
+max_length = len(max(bee_words, key=len))
+longest_words = [word for word in bee_words if \
+              len(word) == max_length]
+
+print("Longest words:", longest_words)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BeeWords
 Finding the longest words that can be used for NYT spelling bee! 
+
+Uses 194,000 word dictionary from http://www.gwicks.net/dictionaries.htm
