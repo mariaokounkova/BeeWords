abc9788c59c1c7e482bdecc1a0ae7fcb6613ec76
Maria Okounkova <m.okounkova@gmail.com>
2023-05-11 17:21:04 -0700

    FindWords.py: Include condition that words must be >= 4 letters
    Include the condition that NYT spelling bee words need to be
    at least 4 letters

--- FindWords.py
+++ FindWords.py
@@ -5,15 +5,14 @@ import numpy as np
 dictionary = np.genfromtxt('dictionary.txt', dtype='str')
 print("%d words in dictionary" % len(dictionary))
 
-## Subset of words without 's' and wirth 7 or fewer letters
+## Subset of words without 's' and with 7 or fewer letters
 bee_words = [word for word in dictionary if \
-             's' not in word and len(set(word)) <= 7]
+             len(word) >= 4 and 's' not in word and len(set(word)) <= 7]
 
 print("%d possible words found" % len(bee_words))
 
 ## Print out the longest words
 max_length = len(max(bee_words, key=len))
-longest_words = [word for word in bee_words if \
-              len(word) == max_length]
+longest_words = [word for word in bee_words if len(word) == max_length]
 
 print("Longest words:", longest_words)
